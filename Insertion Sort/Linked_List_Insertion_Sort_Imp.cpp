a0056b9da4e7643c1da0baf593fdb5acf6982202
jsb58p <jacobbiddinger@gmail.com>
2024-11-07 11:41:58 -0600

    Update Linked_List_Insertion_Sort_Imp.cpp

--- Insertion Sort/Linked_List_Insertion_Sort_Imp.cpp
+++ Insertion Sort/Linked_List_Insertion_Sort_Imp.cpp
@@ -1,28 +1,25 @@
-#include "Linked_List_Insertion_Sort.h"
+void linked_list_insertion_sort::insertion_sort_list(std::list<int>& num) {
+    if (num.size() <= 1) return; // If the list is empty or has only one element, it's already sorted
 
+    auto it = next(num.begin()); // Start from the second element
+    while (it != num.end()) {
+        auto key = *it;  // The element to be inserted
+        auto pos = it;
+        --pos;  // Move to the element before 'it'
 
-void linked_list_insertion_sort::insertion_sort_list(std::list <int>& num) {
-    // If the list has fewer than 2 elements, it's already sorted
-    if (num.size() <= 1) return;
-
-    // Start sorting from the second element
-    for (auto iterator = next(num.begin()); iterator != num.end(); ) {
-        auto key = *iterator;
-        auto currentPos = iterator;
-        auto nextIt = next(iterator);  // Save the next element for the next iteration
-
-        // Move backward through the list and insert key into the sorted part
-        while (currentPos != num.begin() && *prev(currentPos) > key) {
-            currentPos = prev(currentPos);
+        bool insertionNeeded = false;
+        while (pos != num.begin() && key < *pos) {
+            --pos;  // Move backward
+            insertionNeeded = true;
         }
 
-        // If the key is not already in place, insert it at the found position
-        if (currentPos != iterator) {
-            num.erase(iterator);  // Remove the current element
-            num.insert(currentPos, key);  // Insert it in the correct position
+        // If key is not already in the correct place, remove and reinsert it
+        if (insertionNeeded) {
+            num.erase(it);  // Remove the current element
+            num.insert(pos, key);  // Insert it in the correct position
         }
 
         // Move to the next element
-        iterator = nextIt;
+        it = next(it);
     }
 }
