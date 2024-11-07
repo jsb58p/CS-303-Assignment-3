ebe3d6918f085306ee77b926fc6f96de9094e6e8
jsb58p <jacobbiddinger@gmail.com>
2024-11-07 11:41:41 -0600

    Update Main.cpp

--- Insertion Sort/Main.cpp
+++ Insertion Sort/Main.cpp
@@ -3,13 +3,12 @@
 
 int main() {
 	std::list<int> numbers = { 7, 4, 2, 3, 9, 5, 8, 6, 11, 1 };
-	auto front = numbers.begin();
-	linked_list_insertion_sort numbersList;
-	numbersList.insertion_sort_list(numbers);
-	//numbersList.insertion_sort(numbers);
+	linked_list_insertion_sort numbersList;		//declare a linked_list_insertion_sort object to call the insertion sort function
+	numbersList.insertion_sort_list(numbers);	//insertion sort function
+	
 
-	for (const auto& elem : numbers) {
-		std::cout << elem << " ";
+	for (const auto& item : numbers) {  //for loop to display the elements of the list after sorting, to ensure the order is correct
+		std::cout << item << " ";		
 	}
 
 }
