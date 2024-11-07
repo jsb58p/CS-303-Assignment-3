595512dc6137404bbc0e81cb2519801539be7444
jsb58p <jacobbiddinger@gmail.com>
2024-11-07 11:20:24 -0600

    Update Linked_List_Insertion_Sort.h

--- Insertion Sort/Linked_List_Insertion_Sort.h
+++ Insertion Sort/Linked_List_Insertion_Sort.h
@@ -3,8 +3,6 @@
 #define LINKED_LIST_INSERTION_SORT_H_
 #include <list>
 #include <cstddef>
-#include <vector>
-#include <iterator>
 class linked_list_insertion_sort {
 public:
 	void insertion_sort_list(std::list <int>&); //insertion sort function taking a list as a parameter
